1af06438517dae5bcf2c98dc7e6b1d0db4e9a4a0
shirachino <87644665+shirachino@users.noreply.github.com>
2021-08-01 23:56:45 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,7 +1,12 @@
 // 顶部下拉菜单
-var top_li = document.getElementById('top_nav');
-var menu = document.querySelector('.menu');
-console.log(top_li);
-// top_li.children[3].onmouseover = function(){
-// 	menu.style.display = 'block';
-// }
+window.addEventListener('load',function(){
+	var top_li = document.getElementById('top_nav');
+	var menu = document.querySelector('.menu');
+	top_li.children[3].addEventListener('mouseover',function(){
+		menu.style.display = 'block';
+	})
+	top_li.children[3].addEventListener('mouseout',function(){
+		menu.style.display = 'none';
+	})
+})
+
---
+++ js/sysindex.js
@@ -0,0 +1,32 @@
+window.addEventListener('load', function() {
+	// 模态框淡入淡出
+	var mask = document.querySelector('.mask');
+	var modal = document.querySelector('.mydefmodal');
+	var close = document.querySelector('.close');
+	var btndef = document.querySelector('.mydefbtn_default')
+	
+	var btnone = document.querySelector('.mod_one');
+	var fastaddmodal = document.querySelector('.fastadd');
+	
+	btnone.addEventListener('click', function() {
+		mask.style.display = 'block';
+		mask.style.opacity = 1;
+		fastaddmodal.style.display = 'block';
+		fastaddmodal.style.opacity = 1;
+	})
+	// 关闭通用
+	close.addEventListener('click', cls);
+	btndef.addEventListener('click', cls);
+	function cls() {
+		mask.style.animation = 'fadeout .3s';
+		modal.style.animation = 'fadeout .3s';
+		setTimeout(function() {
+			mask.style.display = 'none';
+			modal.style.display = 'none';
+			mask.style.animation = 'fadein .7s';
+			modal.style.animation = 'fadein .5s';
+		}, 300)
+		mask.style.opacity = 0;
+		modal.style.opacity = 0;
+	}
+});
